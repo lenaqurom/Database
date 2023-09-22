84431f9763d7b0bf1eeb3de16566585de3f0322e
lenaqurom <113707663+lenaqurom@users.noreply.github.com>
2023-09-22 22:23:30 +0300

    Add files via upload

---
+++ src/salonproject/SalonProject.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salonproject;
+
+/**
+ *
+ * @author ZBOOK
+ */
+public class SalonProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       new main().setVisible(true);
+    }
+    
+}
